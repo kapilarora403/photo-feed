08a5fca7ae7de7b616a39b089c554ac4ef60080b
kapilarora403 <rishabh@apollo.io>
2021-04-24 19:17:18 +0530

    Config var changed

--- src/actions/photoActions.js
+++ src/actions/photoActions.js
@@ -2,7 +2,7 @@ import { createApi } from "unsplash-js";
 import {ActionTypes} from "../constants/actionTypes";
 
 const unsplash = createApi({
-    accessKey: process.env.PHOTO_ACCESS_KEY,
+    accessKey: process.env.ACCESS_KEY,
 });
 
 export const getQueryResults = ({ query = '' , page = 1, success = null}) => {
